a967c374aa07fbb6e1bdb952624945b7e09c4b38
Stephen Sisk <stephen@Stephen-Sisks-MacBook-Air.local>
2011-04-03 12:09:52 -0700

    Initial Commit

---
+++ Ricochet Solver/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Ricochet Solver
+//
+//  Created by Stephen Sisk on 4/3/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
